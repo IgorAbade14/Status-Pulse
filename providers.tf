43fc74af17dc9ed87827d7e2d4f7d86b7881b253
Abade <abadeti123@gmail.com>
2026-03-23 17:02:54 -0300

    Status Pulse Enable s3 remote backend

--- providers.tf
+++ providers.tf
@@ -6,14 +6,13 @@ terraform {
   # Ensures you are using a modern version of Terraform
   required_version = ">= 1.0.0"
 
-  # BACKEND (REMOTE STATE): 
-  # Uncomment this when you are ready to move from local to AWS S3 storage.
-  # backend "s3" {
-  #   bucket  = "status-pulse-terraform-state-2026" # Updated bucket name
-  #   key     = "production/terraform.tfstate"
-  #   region  = "us-east-1"
-  #   encrypt = true
-  # }
+
+  backend "s3" {
+    bucket  = "status-pulse-terraform-state-2026" # Updated bucket name
+    key     = "production/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
 # Provider AWS: The 'bridge' between your code and Amazon's Cloud
